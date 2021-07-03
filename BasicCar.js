bcf2d812d687d5df0501ef24e851673231d0bd02
somyeong <thaud8487@naver.com>
2021-07-03 16:09:46 +0900

    네번째 커밋

---
+++ BasicCar.js
@@ -0,0 +1,13 @@
+class BasicCar{
+    constructor(color, maxSpeed,price){
+        this.color=color;
+        this.maxSpeed=maxSpeed;
+        this.price=price;
+    }
+
+
+run() {
+    console.log('Running...');
+
+}
+}

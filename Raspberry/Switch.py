f06f7486be5368b6058563acb8f63069472baf69
배현철 <bhc2675@gmail.com>
2021-07-19 00:03:47 +0900

    Upload New File

---
+++ Raspberry/Switch.py
@@ -0,0 +1,19 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(18,GPIO.IN,pull_up_down=FPIO.PUP_UP)
+
+while True:
+    try:
+        button_state =GPIO.input(18)
+        if button_state==False:
+            print('Button ON')
+            time.sleep(0.2)
+
+    except KeyboardInterrupt:
+        pass
+        print('Exit with ^C. Goodbye!')
+        GPIO.cleanup()
+        exit()

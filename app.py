fb446b96c19b8a38f82967b3b9be8e29480cf5e8
이소명 <thaud8487@naver.com>
2021-07-23 13:34:59 +0900

    제이쿼리 코드 위치 수정

--- app.py
+++ app.py
@@ -21,22 +21,22 @@ def show_menu():
     print(sample_receive)
     return jsonify({'msg': 'GET 연결 완료!'})
 
-# # 주문하기(POST) API
-# @app.route('/order', methods=['POST'])
-# def save_order():
-#     sample_receive = request.form['sample_give']
-#     print(sample_receive)
-#     # menu_list_receive = request.form['menu_list_give']
-#     # total_price_receive=request.form['total_price_give']
-#     #
-#     # doc={
-#     #     'menu_list': menu_list_receive,
-#     #     'total_price': total_price_receive
-#     # }
-#     #
-#     # db.orders.insert_one(doc)
-#
-#     return jsonify({'msg': 'POST 요청 완료되었습니다!'})
+# 주문하기(POST) API
+@app.route('/pos', methods=['POST'])
+def save_order():
+    sample_receive = request.form['sample_give']
+    print(sample_receive)
+    # menu_list_receive = request.form['menu_list_give']
+    # total_price_receive=request.form['total_price_give']
+    #
+    # doc={
+    #     'menu_list': menu_list_receive,
+    #     'total_price': total_price_receive
+    # }
+    #
+    # db.orders.insert_one(doc)
+
+    return jsonify({'msg': 'POST 요청 완료되었습니다!'})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)

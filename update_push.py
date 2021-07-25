1a89e616204118026d7ff6b2aa0b3d1b5cbdfaad
이소명 <thaud8487@naver.com>
2021-07-26 03:24:19 +0900

    mongodb에서 document안의 원소안에 리스트 추가하는법 찾았다!

---
+++ update_push.py
@@ -0,0 +1,31 @@
+#Working with mongoDB Atlas using Python
+#유툽 참고
+
+from pymongo import MongoClient
+# # client=MongoClient("mongodb+srv://somyeong:teentop0816@cluster0.dluuf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+client=MongoClient('localhost', 27017)
+db=client.mydb
+test=db.test
+
+## doc안의 원소안에 리스트 추가하는 법
+
+doc={
+    "o_s_id":1,
+    "table_no":2,
+    "menu":[{
+        "id":1,
+        "name":"김치찌개",
+        "count":2
+    }]
+}
+# db.menu.insert(doc)
+db.menu.update(
+    {"o_s_id":1},
+    {
+ "$push":{
+        "menu":[
+           {"id":3, "name":"짬뽕","count":1}
+        ]
+    }
+})
+

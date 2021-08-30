1ec3136908c53e13ee1e4669df30b6115822f637
배현철 <bhc2675@gmail.com>
2021-08-30 19:37:54 +0900

    Replace Button_To_DB.py

--- Raspberry/GPIO/Button_To_DB.py
+++ Raspberry/GPIO/Button_To_DB.py
@@ -2,10 +2,9 @@ from pymongo import MongoClient
 import RPi.GPIO as GPIO
 import time
 
-client = MongoClient("mongodb+srv://Berrykind:<password>@cluster0.z7rql.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+client = MongoClient("mongodb+srv://Berrykind:poseidon@cluster0.z7rql.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 
 def CalNexts_id(self) :
-    max = int()
     max = 0
     x = self.find().sort("s_id")
     for i in x:
@@ -17,9 +16,12 @@ db = client.get_database('Serving_Robot')
 Center = db.Center
 i = int()
 
+GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
-
 GPIO.setup(18,GPIO.IN,pull_up_down=GPIO.PUD_UP)
+
+GPIO.setup(17, GPIO.OUT)
+p = GPIO.PWM(17, 100)
 
 while True:
     try:
@@ -29,7 +31,16 @@ while True:
             i = CalNexts_id(Center)
             data={'s_id' : i, 'table_no' : 1, 'sig' : 1, 'now_work' : 1}
             Center.insert_one(data)
-            time.sleep(0.2)
+
+            p.start(100)
+            p.ChangeDutyCycle(90)
+
+            p.ChangeFrequency(699)
+            time.sleep(.2)
+            p.ChangeFrequency(880)
+            time.sleep(.3)
+
+            p.stop()
 
     except KeyboardInterrupt:
         pass
